275fcecff125b4dfac23ac97683662367a6a495f
Ali Churcher <churcher.ali@gmail.com>
2023-01-08 20:51:10 +1300

    assign new friends to current user

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,7 +4,6 @@ class FriendsController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
   # GET /friends or /friends.json
   def index
-    # @friends = Friend.where(user_id: current_user.id)
     @friends = Friend.all
   end
   # GET /friends/1 or /friends/1.json
@@ -13,7 +12,7 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -22,7 +21,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    # @friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
